e11057645e765aa2ba843e1149b3795d12943d90
Ludgil <gilotaux.lud@gmail.com>
2020-01-24 10:16:23 +0100

    before get vincent acf

--- templatebecode/front-page.php
+++ templatebecode/front-page.php
@@ -7,13 +7,63 @@
             <img src="<?php echo get_template_directory_uri().'/assets/images/1500x1250_01.jpg';?>" alt="photo">
             <div class="box-left">
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita tenetur a facilis ab adipisci est!</p>
+                <a href="#">>></a>
             </div>
         </div>
         <div class="right-side">
             <img src="<?php echo get_template_directory_uri().'/assets/images/1500x1250_02.jpg';?>" alt="photo">
             <div class="box-right">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis minima voluptas sunt quas. Eum, quidem.</p>
+                <a href="#">>></a>
             </div>
+        </div>
+    </section>
+    <section id="intro">
+        <div id="intro-content">
+            <div class="left-side">
+            <div id="box-design"></div>
+                <div class="title-intro">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita tenetur a facilis ab adipisci est!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex saepe excepturi laboriosam voluptate sapiente possimus dignissimos officia itaque,
+                    </p>
+                </div>
+                <div class="content-intro">
+                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, nostrum eligendi beatae quasi enim provident.</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quos itaque quia, molestiae accusamus excepturi inventore ducimus cum ea vero odit? 
+                    Cumque aut numquam incidunt atque exercitationem quos vero facilis ducimus, laborum esse 
+                    fugiat ratione iusto. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ullam 
+                    quia dolor, modi quod beatae officiis corrupti
+                     corporis eius sequi fugit est sunt at ad maiores sint expedita ea? Unde! Lorem, 
+                     ipsum dolor sit amet consectetur adipisicing elit. Provident, ad ipsum quidem pariatur facilis, sed minima mollitia assumenda quae repellendus dicta delectus unde rem voluptatibus, eligendi consectetur labore quo ullam recusandae ea. Repellendus quas quae autem, animi sint esse hic tempora quidem 
+                     quo corporis vitae odit culpa, neque facere officia.
+                    </p>
+
+                </div>
+            </div>
+            <div class="right-side">
+                <img src="<?php echo get_template_directory_uri().'/assets/images/1000x1600_01.jpg';?>" alt="photo">
+            </div>
+        </div>
+    </section>
+    <section id="milieu">
+        <div id="box-milieu"></div>
+        <div class="milieu-content">
+            <img src="<?php echo get_template_directory_uri().'/assets/svg/logo-small.svg';?>" alt="logo-small">
+            <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores iste totam asperiores, incidunt modi, possimus facere architecto suscipit obcaecati nostrum natus voluptates blanditiis numquam quia necessitatibus et similique delectus 
+                molestias dolorum, ullam sed laudantium enim labore repudiandae! Eaque, quas ratione?</h2>
+            <a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, est?</a>
+        </div>
+    </section>
+    <section id="tissu">
+        <div class="left-side">
+            <img src="<?php echo get_template_directory_uri().'/assets/images/1850x1200_01.jpg';?>" alt="">
+        </div>
+        <div class="right-side">
+            <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam laboriosam fuga sunt in similique officia.</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui sunt hic ducimus tempora obcaecati similique, eum cupiditate ad impedit aspernatur facere, rem deleniti ipsum iure debitis quae consectetur iste commodi reprehenderit! Delectus alias facilis quaerat fuga reprehenderit! Ullam perferendis possimus quidem inventore, magnam iure harum tempora nostrum in sequi laborum, minus non quod quo accusamus dicta,
+                 impedit suscipit! Reiciendis enim optio temporibus sint placeat illo!</p>
+            <a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, dignissimos.</a>
         </div>
     </section>
 
