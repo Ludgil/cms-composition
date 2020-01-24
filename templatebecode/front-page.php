43172b226ad899864a3dacae9601c3cbdc4d7ea8
Ludgil <gilotaux.lud@gmail.com>
2020-01-24 12:07:50 +0100

    want recup

--- templatebecode/front-page.php
+++ templatebecode/front-page.php
@@ -6,26 +6,27 @@
 $bandeau = get_field('bandeau');
 $intro = get_field('intro');
 $milieu = get_field('milieu');
-$tissus = get_field('tissus')
+$tissus = get_field('tissus');
 
 ?>
 
-<? get_header() ?>
+<?php get_header(); ?>
+
     <section id="bandeau">
         <div class="left-side">
-            <img src="<? echo $bandeau["img-1"]?>">
+            <img src="<?php echo $bandeau["img-1"];?>" alt="">
             <div class="box-left">
-                <h2><? echo $bandeau["titre-1"]?></h2>
-                <h2><? echo $bandeau["hover-1"]["titre"]?></h2>
-                <p><? echo $bandeau["hover-1"]["texte"]?></p>
+                <h2><?php echo $bandeau["titre-1"];?></h2>
+                <h2><?php echo $bandeau["hover-1"]["titre"];?></h2>
+                <p><?php echo $bandeau["hover-1"]["texte"];?></p>
             </div>
         </div>
         <div class="right-side">
-            <img src="<? echo $bandeau["img-2"] ?>" alt="">
+            <img src="<?php echo $bandeau["img-2"]; ?>" alt="">
             <div class="box-right">
-                <h2><? echo $bandeau["titre-2"]?></h2>
-                <h2><? echo $bandeau["hover-2"]["titre"]?></h2>
-                <p><? echo $bandeau["hover-2"]["texte"]?></p>
+                <h2><?php echo $bandeau["titre-2"];?></h2>
+                <h2><?php echo $bandeau["hover-2"]["titre"];?></h2>
+                <p><?php echo $bandeau["hover-2"]["texte"];?></p>
             </div>
         </div>
     </section>
@@ -82,26 +83,26 @@ $tissus = get_field('tissus')
     <!-- INTRO -->
 
 
-    <p><? echo $intro["texte"]?></p>
-    <h2><? echo $intro["titre"]?></h2>
-    <p><? echo $intro["wysiwyg"]?></p>
-    <a href="<? echo $intro["lien_interne"]["url"]?>"><? echo $intro["lien_interne"]["title"]?></a>
-    <img src="<? echo $intro["img"]?>">
+    <p><?php echo $intro["texte"];?></p>
+    <h2><?php echo $intro["titre"];?></h2>
+    <p><?php echo $intro["wysiwyg"];?></p>
+    <a href="<?php echo $intro["lien_interne"]["url"];?>"><? echo $intro["lien_interne"]["title"]?></a>
+    <img src="<?php echo $intro["img"];?>">
 
     <!-- MILIEU -->
 
 
-    <p><? echo $milieu["texte"]?></p>
-    <a href="<? echo $milieu["lien_interne"]["url"]?>"><? echo $milieu["lien_interne"]["title"]?></a>
+    <p><?php echo $milieu["texte"];?></p>
+    <a href="<?php echo $milieu["lien_interne"]["url"];?>"><?php echo $milieu["lien_interne"]["title"];?></a>
 
 
     <!-- TISSUS -->
 
 
-    <img src="<? echo $tissus["img"]?>" alt="">
-    <h2><? echo $tissus["titre"]?></h2>
-    <p><? echo $tissus["wysiwyg"]?></p>
-    <a href="<? echo $tissus["lien_interne"]["url"]?>"><? echo $tissus["lien_interne"]["title"]?></a>
+    <img src="<?php echo $tissus["img"];?>" alt="">
+    <h2><?php echo $tissus["titre"];?></h2>
+    <p><?php echo $tissus["wysiwyg"];?></p>
+    <a href="<?php echo $tissus["lien_interne"]["url"];?>"><?php echo $tissus["lien_interne"]["title"];?></a>
     
 
 
